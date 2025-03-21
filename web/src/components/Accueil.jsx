14e452b0d799a55c01b54000891bba234455d763
Mohamed <mohamedcoulibaly66201@gmail.com>
2025-03-21 21:25:46 +0000

    ville deserv

--- web/src/components/Accueil.jsx
+++ web/src/components/Accueil.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 function Accueil({ onReserveClick }) {
   return (
@@ -14,11 +13,13 @@ function Accueil({ onReserveClick }) {
             <img src="img/rafraf.jpg" className="img-fluid w-100" alt="First slide" />
             <div className="carousel-caption">
               <div className="container py-4">
-                <div className="row g-5">
-                  <div className="col-lg-6 fadeInLeft animated" style={{ animationDelay: "1s" }}>
-                    <div className="bg-secondary rounded p-5 d-flex flex-column align-items-start">
-                      <h4 className="text-white mb-4 text-start">LES VILLES QUE NOUS DESSERVONS</h4>
-                      <ul className="list-unstyled text-white fs-5 text-start ps-1">
+                <div className="row g-4 align-items-center">
+                  
+                  {/* Section Liste des Villes */}
+                  <div className="col-12 col-md-6 fadeInLeft animated" style={{ animationDelay: "1s" }}>
+                    <div className="bg-secondary rounded p-4 w-100 text-start">
+                      <h4 className="text-white mb-3">LES VILLES QUE NOUS DESSERVONS</h4>
+                      <ul className="list-unstyled text-white fs-5">
                         <li className="mb-2"><i className="bi bi-geo-alt-fill me-2"></i>ABIDJAN</li>
                         <li className="mb-2"><i className="bi bi-geo-alt-fill me-2"></i>BOUAKE</li>
                         <li className="mb-2"><i className="bi bi-geo-alt-fill me-2"></i>BOUNDIALI</li>
@@ -26,20 +27,20 @@ function Accueil({ onReserveClick }) {
                         <li className="mb-2"><i className="bi bi-geo-alt-fill me-2"></i>ODIENE</li>
                       </ul>
                     </div>
+                    
                   </div>
 
-                  <div className="col-lg-6 d-none d-lg-flex fadeInRight animated" style={{ animationDelay: "1s" }}>
-                    <div className="text-start">
-                      <h1 className="display-5 text-white">Voyagez sereinement avec RafRaf – Confort et sécurité garantis !</h1>
-                      <button 
-                        className="btn btn-primary rounded-pill py-2 px-4 me-2"
-                        onClick={onReserveClick}
-                      >
-                        Reserver
-                      </button>
-                    </div>
+                  {/* Section Texte + Bouton */}
+                  <div className="col-12 col-md-6 fadeInRight animated text-center text-md-start" style={{ animationDelay: "1s" }}>
+                    <h1 className="display-6 text-white">
+                      Voyagez sereinement avec RafRaf <br className="d-none d-md-block" /> Confort et sécurité garantis !
+                    </h1>
+                    <button className="btn btn-primary rounded-pill py-2 px-4 mt-3" onClick={onReserveClick}>
+                      Réserver
+                    </button>
                   </div>
-                </div>
+
+                </div>  
               </div>
             </div>
           </div>
