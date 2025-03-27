9cc02b566116082b5774e82d8080dd4ad70f94f7
Mohamed <mohamedcoulibaly66201@gmail.com>
2025-03-27 21:37:38 +0000

    node module backend

--- web/src/components/Destination.js
+++ web/src/components/Destination.js
@@ -19,26 +19,30 @@ const destinations = [
               </p>
             </div>
             <div className="row g-4">
-              {destinations.map((destination, index) => (
-                <div key={destination.id} className="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay={`${0.1 + index * 0.2}s`}>
-                  <div className="team-item p-3 pt-0 d-flex flex-column h-100" style={{ height: '350px' }}>
-                    <div className="team-img">
-                      <img 
-                        src={`img/ville-${destination.id}.jpg`} 
-                        className="img-fluid rounded w-100" 
-                        alt={destination.name} 
-                        style={{ height: '150px', objectFit: 'cover' }}
-                      />
-                    </div>
-                    <div className="team-content pt-3 d-flex flex-column flex-grow-1">
-                      <h4 className="text-center">{destination.name}</h4>
-                      <p className="text-center flex-grow-1">Découvrez {destination.name}, une destination incontournable en Côte d'Ivoire.</p>
-                      <button className="btn btn-primary mt-auto">Réserver</button>
+                {destinations.map((destination, index) => (
+                  <div 
+                    key={destination.id} 
+                    className="col-12 col-sm-6 col-lg-6 col-xl-3 animate__animated animate__fadeInUp" 
+                    style={{ animationDelay: `${0.1 + index * 0.2}s` }}
+                  >
+                    <div className="team-item p-3 pt-0 d-flex flex-column h-100" style={{ height: '350px' }}>
+                      <div className="team-img">
+                        <img 
+                          src={`img/ville-${destination.id}.jpg`} 
+                          className="img-fluid rounded w-100" 
+                          alt={destination.name} 
+                          style={{ height: '150px', objectFit: 'cover' }}
+                        />
+                      </div>
+                      <div className="team-content pt-3 d-flex flex-column flex-grow-1">
+                        <h4 className="text-center">{destination.name}</h4>
+                        <p className="text-center flex-grow-1">Découvrez {destination.name}, une destination incontournable en Côte d'Ivoire.</p>
+                        <button className="btn btn-primary mt-auto">Réserver</button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
           </div>
         </div>
       </div>
